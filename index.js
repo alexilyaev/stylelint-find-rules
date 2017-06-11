80983392d44f9f64406e9551cadd2c3967f69976
Alex Ilyaev <alexilyaev@gmail.com>
2017-06-12 01:56:56 +0300

    lets see if pre-commit catches this

---
+++ index.js
@@ -0,0 +1,14 @@
+const one = 1;
+
+let obj = { one: one };
+
+function doSomething() {
+  return [
+    Object.keys(obj),
+    Object.keys(obj),
+    Object.keys(obj),
+    Object.keys(obj),
+    Object.keys(obj),
+    Object.keys(obj)
+  ];
+}
