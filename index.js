dbce0aa330c271d6cfc192a4436b7dcad839d487
Alex Ilyaev <alexilyaev@gmail.com>
2017-06-12 02:07:17 +0300

    prettier using pre-commit hook

--- index.js
+++ index.js
@@ -1,14 +1,2 @@
 const one = 1;
-
 let obj = { one: one };
-
-function doSomething() {
-  return [
-    Object.keys(obj),
-    Object.keys(obj),
-    Object.keys(obj),
-    Object.keys(obj),
-    Object.keys(obj),
-    Object.keys(obj)
-  ];
-}
