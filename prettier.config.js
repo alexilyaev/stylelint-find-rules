c09dfa0353665dbc0781a68696c487daedbd9a37
Alex Ilyaev <alexilyaev@gmail.com>
2017-09-21 03:02:42 +0300

    Add prettier config file, update ESLint config

---
+++ prettier.config.js
@@ -0,0 +1,12 @@
+'use strict';
+
+module.exports = {
+  bracketSpacing: true,
+  jsxBracketSameLine: false,
+  printWidth: 100,
+  semi: true,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: 'none',
+  useTabs: false
+};
